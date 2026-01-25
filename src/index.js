e0036b742dc0a53efce783fa7d258bc78060d29a
Paul Fernandez <paul.fernandez@pwccmarketplace.com>
2026-01-25 15:47:20 -0800

    CSS layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { body, div, h1, h2, head, header, html,
-         link, main, meta, render, section, title } from '@pfern/elements'
+import './style.css'
+import { a, article, aside, body, div, h1, h2, head, header, html, li, main,
+  meta, nav, render, section, summary, title, ul} from '@pfern/elements'
 import { counter } from './components/counter.js'
 import { todos } from './components/todos.js'
 
@@ -7,21 +8,30 @@ render(
   html(
     head(
       title('elements.js'),
-      meta({ name: 'viewport',
-             content: 'width=device-width, initial-scale=1.0' }),
-      link({ rel: 'stylesheet', href: 'src/style.css' })),
-    body(
+      meta({ charset: 'utf-8' }),
+      meta({ name: 'viewport', content: 'width=device-width, initial-scale=1' }),
+      meta({ name: 'color-scheme', content: 'light dark' })
+    ),
+    body({ class: 'container' },
       header(
-        h1('Elements.js Demo')),
-      main(
-        section(
-          h2('Todos'),
-          todos()),
-        section({ class: 'grid' },
-          div(
-            h2('Counter 1'),
-            counter()),
-          div(
-            h2('Counter 2'),
-            counter()))))))
+        h1(undefined, 'Elements.js Demo')),
+      main({ class: 'grid' },
+        aside(
+          nav( // Wrap summary & ul in `details()` to expand/collapse lists.
+            summary('Posts'),
+            ul(
+              li(a({ href: '#' }, 'This is a link.')),
+              li(a({ href: '#' }, 'This is a link.'))
+            ))),
+        article(
+          section(
+            h2('Todos'),
+            todos()),
+          section({ class: 'grid' },
+            div(
+              h2('Counter 1'),
+              counter()),
+            div(
+              h2('Counter 2'),
+              counter())))))))
 
