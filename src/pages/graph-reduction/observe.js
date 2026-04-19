ad39e905b38f3adf222c31c44f6a4fc4cb36fbfc
Paul Fernandez <paul4nandez@gmail.com>
2026-04-19 11:16:30 -0700

    WIP

--- src/pages/graph-reduction/observe.js
+++ src/pages/graph-reduction/observe.js
@@ -1,6 +1,7 @@
 const isAtom = node => !Array.isArray(node)
 const isEmpty = node => Array.isArray(node) && node.length === 0
 const isSelf = (a, b) => a === b
+const isPair = node => Array.isArray(node) && node.length === 2
 
 
 /**
@@ -18,7 +19,17 @@ export const observe = root => {
   const next = observe(first)
 
   if (!isSelf(next, first)) return [next, rest]
+  if (!isAtom(first)) {
+    const nextRest = observe(rest)
+    if (!isSelf(nextRest, rest)) return [first, nextRest]
+  }
+
+  if (isPair(first) && isPair(rest) && isSelf(first[1], rest[1])) {
+    const nextRest = observe(first[1])
+    if (!isSelf(nextRest, first[1])) {
+      return [[first[0], nextRest], [rest[0], nextRest]]
+    }
+  }
 
   return root
 }
-
