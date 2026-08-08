10fab8a195066b8b861b4a1910f2bb523ce7d582
Paul Fernandez <paul4nandez@gmail.com>
2026-08-08 16:41:57 -0700

    Extract fix and isSymbol

--- src/pages/graph-reduction/graph/link.js
+++ src/pages/graph-reduction/graph/link.js
@@ -6,31 +6,41 @@ const find = (symbol, stack, i = stack.length - 1) =>
   i >= 0 && (stack[i].find(node => node?.symbol === symbol)
     ?? find(symbol, stack, i - 1))
 
+const fix = (graph, symbol, ...rest) => {
+  graph[0] = graph
+
+  if (rest.length) graph.push(...rest)
+  else graph[1] = graph
+
+  graph['symbol'] = symbol
+  return graph
+}
+
+const isSymbol = node => typeof node === 'string'
+
 const fold = (tree, stack = []) => {
   if (!Array.isArray(tree)) return
   const graph = []
   stack = [...stack, graph]
 
   tree.forEach((node, i) => {
-    if (typeof node === 'string') {
+    if (isSymbol(node)) {
       const ref = find(node, stack)
 
       if (i === 0) {
-        graph[i] = ref || graph
-        graph['symbol'] = node
+        if (ref) graph[i] = ref
+        else fix(graph, node, ...tree.slice(1))
       } else if (ref) {
         graph[i] = ref
       } else {
-        const atom = []
-        graph[i] = atom[0] = atom[1] = atom
-        atom['symbol'] = node
-        Object.freeze(atom)
+        const atom = fix([], node)
+        graph[i] = Object.freeze(atom)
       }
     } else {
       const branch = fold(node, stack)
       graph[i] = branch
 
-      if (i === 1 && graph['symbol'])
+      if (i === 1 && isSymbol(tree[0]))
         stack = [...stack, branch]
     }
   })
